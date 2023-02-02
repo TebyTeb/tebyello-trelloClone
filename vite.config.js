c75ee575bec1bcf70f2b27ff5a41ec1514683d43
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-02-02 21:20:00 +0000

    gh-pages prep

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/tebyello-trelloClone/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
